ff1dbb3e4ad169d27d4292804aa2d90c8bbe24bb
cavsang <33303073+cavsang@users.noreply.github.com>
2018-03-11 21:47:44 +0900

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,3 +1,8 @@
 //For command line git tutorial
-//Local repository modification
-//
+//Remote repository modification
+
+public class hello{
+  public static void main(String[] args){
+    System.out.println("hello"); 
+  }
+}
